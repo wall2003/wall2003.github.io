ef4ed28819e37b88665898bf8f458038f8d87419
Pavel Fokin <2268651@gmail.com>
2014-10-14 00:20:37 +0300

    fix error

--- Themes/Default/main-body.js
+++ Themes/Default/main-body.js
@@ -5,6 +5,7 @@ document.write('<script src="http://wall2003.github.io/Themes/Default/app-app.js
 document.write('<script src="http://wall2003.github.io/Themes/Default/directives/app-directives.js"></script>');
 document.write('<script src="http://wall2003.github.io/Themes/Default/controllers/app-controllers.js"></script>');
 
+document.write('<script src="http://wall2003.github.io/js/jquery-1.10.2.min.js"></script>');
 document.write('<script src="http://wall2003.github.io/js/bootstrap.min.js"></script>');
 document.write('<script src="http://wall2003.github.io/js/application.js"></script>');
 document.write('<script src="http://wall2003.github.io/js/offcanvas.js"></script>');
