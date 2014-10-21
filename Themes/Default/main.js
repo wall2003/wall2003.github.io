29bbdced26c01187b43ca95bcb8540289289c6b3
Pavel Fokin <2268651@gmail.com>
2014-10-21 23:31:59 +0300

    test

--- Themes/Default/main.js
+++ Themes/Default/main.js
@@ -100,7 +100,13 @@
 
     //template
     //root-container
-    document.write('<script src="http://wall2003.github.io/Themes/Default/templates/root-container.js"></script>');
+    document.write('<script id="root-container.html" type="text/ng-template">' +
+        '<div class="row row-offcanvas row-offcanvas-right"><div class="col-xs-12 col-sm-3 col-md-2" data-ng-controller="leftCtrl">' +
+        '<div class="navbar-header" navbar-header></div><div class="navbar-collapse in" navbar-collapse></div></div>' +
+        '<div class="content col-xs-12 col-sm-5 col-md-7" data-ng-controller="centerCtrl"><h4 data-ng-bind="menu"></h4></div>' +
+        '<div class="col-xs-6 col-sm-4 col-md-3 sidebar-offcanvas" id="sidebar" data-ng-controller="rightCtrl">' +
+        '<h4 data-ng-bind="menu"></h4></div></div>' +
+        '</script>');
     //menu-widget
     document.write('<script src="http://wall2003.github.io/Themes/Default/templates/menu-widget.js"></script>');
 
