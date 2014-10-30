daf016c08d2d3ab7bfb68eb110d65a29b9831500
Pavel Fokin <2268651@gmail.com>
2014-10-30 21:58:24 +0300

    test

---
+++ Widgets/Menu/menu-widget.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var menuWidget = angular.module('menuWidget', [])
+    .controller('menuWidgetCtrl', ['$scope', function($scope){
+        $scope.title = 'The story of the cat';
+        var list = JSON.parse($('#content-linked-data').html());
+        var tt=0;
+    }])
+    .directive('menuWidget', function(){
+        return {
+            restrict: 'A',
+            templateUrl: 'menu-widget.html'
+        };
+    });
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 (function(){
     //prepare angular root element
     var html = document.documentElement;
-    html.setAttribute('data-ng-app', 'app');
-    html.setAttribute('data-ng-controller', 'appCtrl');
+    //html.setAttribute('data-ng-app', 'app');
+    //html.setAttribute('data-ng-controller', 'appCtrl');
     html.setAttribute('lang', 'en');
 
     //
@@ -86,25 +86,47 @@
     var body = document.getElementsByTagName('body')[0];
     var cont = document.createElement('div');
     cont.className = 'container-liquid';
-    cont.setAttribute('container', '');
-    //body.appendChild(cont);
+    body.appendChild(cont);
     var scripts = document.getElementById('root_script');
     body.insertBefore(cont, scripts[0]);
 
+    //container - div
+    var div = document.createElement('div');
+    div.className = 'row row-offcanvas row-offcanvas-right';
+    cont.appendChild(div);
+
+    //container - div - div1 - cont
+    var div1 = document.createElement('div');
+    div1.className = 'col-xs-12 col-sm-3 col-md-2';
+    div.appendChild(div1);
+    cont = document.createElement('div');
+    cont.className = 'navbar navbar-inverse main navbar-fixed-top row-offcanvas-menu';
+    div1.appendChild(cont);
+
+    //container - div - div1
+    div1 = document.createElement('div');
+    div1.className = 'content col-xs-12 col-sm-5 col-md-7';
+    div.appendChild(div1);
+
+    //container - div - div1
+    div1 = document.createElement('div');
+    div1.className = 'col-xs-6 col-sm-4 col-md-3 sidebar-offcanvas';
+    div1.setAttribute('id', 'sidebar');
+    div.appendChild(div1);
 
     //load scripts
-    document.write("<script src='//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>");
-    document.write("<script src='//ajax.googleapis.com/ajax/libs/angularjs/1.2.22/angular.min.js'></script>");
-    document.write('<script src="http://wall2003.github.io/js/bootstrap.min.js"></script>');
-    document.write('<script src="http://wall2003.github.io/js/offcanvas.js"></script>');
-    document.write('<script src="http://wall2003.github.io/js/application.js"></script>');
+//    document.write("<script src='//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>");
+//    document.write("<script src='//ajax.googleapis.com/ajax/libs/angularjs/1.2.22/angular.min.js'></script>");
+//    document.write('<script src="http://wall2003.github.io/js/bootstrap.min.js"></script>');
+//    document.write('<script src="http://wall2003.github.io/js/offcanvas.js"></script>');
+//    document.write('<script src="http://wall2003.github.io/js/application.js"></script>');
 
     //application
     var skin = 'Default';
-    document.write('<script src="http://wall2003.github.io/Themes/' + skin +'/app-app.js"></script>');
-    document.write('<script src="http://wall2003.github.io/Themes/' + skin +'/templates.js"></script>');
-    document.write('<script src="http://wall2003.github.io/Themes/' + skin +'/directives/app-directives.js"></script>');
-    document.write('<script src="http://wall2003.github.io/Themes/' + skin +'/controllers/app-controllers.js"></script>');
+//    document.write('<script src="http://wall2003.github.io/Themes/' + skin +'/app-app.js"></script>');
+//    document.write('<script src="http://wall2003.github.io/Themes/' + skin +'/templates.js"></script>');
+//    document.write('<script src="http://wall2003.github.io/Themes/' + skin +'/directives/app-directives.js"></script>');
+//    document.write('<script src="http://wall2003.github.io/Themes/' + skin +'/controllers/app-controllers.js"></script>');
 
 
 
