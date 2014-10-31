d4f303287c8d4bf40a9b8d75eae39306dbb65b20
Pavel Fokin <2268651@gmail.com>
2014-10-31 22:03:51 +0300

    test

--- Widgets/Menu/menu-widget.js
+++ Widgets/Menu/menu-widget.js
@@ -3,10 +3,10 @@
 var menuWidget = angular.module('menuWidget', [])
     .controller('menuWidgetCtrl', ['$scope', function($scope){
         $scope.title = 'The story of the cat';
-        var list = 0;
         var tt=0;
-    }])
-    .directive('menuWidget', function(){
+    }]);
+
+menuWidget.directive('menuWidget', function(){
         return {
             restrict: 'E',
             templateUrl: 'menu-widget.html'
--- main.js
+++ main.js
@@ -137,7 +137,7 @@
 //    document.write('<script src="http://wall2003.github.io/Themes/' + skin +'/controllers/app-controllers.js"></script>');
 
 
-    document.write('<script>$(document).ready(function() {var Webrunes = Webrunes || {};Webrunes.Menu = $("#content-linked-data").html();});</script>');
+    document.write('<script>$(document).ready(function() {var WMW = $("#content-linked-data").html();});</script>');
     document.write('<script>$(document).ready(function() {alert("document ready");});</script>');
 
 
