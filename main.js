bfd659a475571c3418ed1397ddc4ff3bac5c8b0b
Pavel Fokin <2268651@gmail.com>
2014-11-04 23:28:53 +0300

    test

--- main.js
+++ main.js
@@ -6,23 +6,59 @@
 
     //default theme
     var theme = 'Default';
-    //add css
-    document.write("<link src='http://wall2003.github.io/Themes/" + theme + "/css/bootstrap.min.css' rel='stylesheet'/>");
-    document.write("<link src='http://wall2003.github.io/Themes/" + theme + "/css/webrunes.css' rel='stylesheet'/>");
-    document.write("<link src='http://wall2003.github.io/Themes/" + theme + "/ico/favicon.ico' rel='shortcut icon'/>");
-    document.write("<link src='http://wall2003.github.io/Themes/" + theme + "/ico/apple-touch-icon-144-precomposed.png' rel='apple-touch-icon-precomposed' sizes='144x144'/>");
-    document.write("<link src='http://wall2003.github.io/Themes/" + theme + "/ico/apple-touch-icon-114-precomposed.png' rel='apple-touch-icon-precomposed' sizes='114x114'/>");
-    document.write("<link src='http://wall2003.github.io/Themes/" + theme + "/ico/apple-touch-icon-72-precomposed.png' rel='apple-touch-icon-precomposed' sizes='72x72'/>");
-    document.write("<link src='http://wall2003.github.io/Themes/" + theme + "/ico/apple-touch-icon-57-precomposed.png' rel='apple-touch-icon-precomposed'/>");
-    //scripts
-    document.write("<script src='//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>");
-    document.write("<script src='//ajax.googleapis.com/ajax/libs/angularjs/1.2.22/angular.min.js'></script>");
+
+
+
+    //add to head
+    var head = document.getElementsByTagName('head')[0];
+    var meta = document.createElement('link');
+    meta.setAttribute('href', 'http://wall2003.github.io/Themes/'  + theme + '/css/bootstrap.min.css');
+    meta.setAttribute('rel', 'stylesheet');
+    head.appendChild(meta);
+
+    meta = document.createElement('link');
+    meta.setAttribute('href', 'http://wall2003.github.io/Themes/'  + theme + '/css/webrunes.css');
+    meta.setAttribute('rel', 'stylesheet');
+    head.appendChild(meta);
+
+    meta = document.createElement('link');
+    meta.setAttribute('href', 'http://wall2003.github.io/Themes/'  + theme + '/ico/favicon.ico');
+    meta.setAttribute('rel', 'shortcut icon');
+    head.appendChild(meta);
+
+    meta = document.createElement('link');
+    meta.setAttribute('rel', 'apple-touch-icon-precomposed');
+    meta.setAttribute('sizes', '144x144');
+    meta.setAttribute('href', 'http://wall2003.github.io/Themes/'  + theme + '/ico/apple-touch-icon-144-precomposed.png');
+    head.appendChild(meta);
+
+    meta = document.createElement('link');
+    meta.setAttribute('rel', 'apple-touch-icon-precomposed');
+    meta.setAttribute('sizes', '114x114');
+    meta.setAttribute('href', 'http://wall2003.github.io/Themes/'  + theme + '/ico/apple-touch-icon-114-precomposed.png');
+    head.appendChild(meta);
+
+    meta = document.createElement('link');
+    meta.setAttribute('rel', 'apple-touch-icon-precomposed');
+    meta.setAttribute('sizes', '72x72');
+    meta.setAttribute('href', 'http://wall2003.github.io/Themes/'  + theme + '/ico/apple-touch-icon-72-precomposed.png');
+    head.appendChild(meta);
+
+    meta = document.createElement('link');
+    meta.setAttribute('rel', 'apple-touch-icon-precomposed');
+    meta.setAttribute('href', 'http://wall2003.github.io/Themes/'  + theme + '/ico/apple-touch-icon-57-precomposed.png');
+    head.appendChild(meta);
+
+    //load scripts
+    var script = document.createElement('script');
+    script.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js');
+    head.appendChild(script);
+
+    script = document.createElement('script');
+    script.setAttribute('src', '//ajax.googleapis.com/ajax/libs/angularjs/1.2.22/angular.min.js');
+    head.appendChild(script);
+
 
     //prepare container
     document.write("<script src='http://wall2003.github.io/prepare.container.js'></script>");
-
-//    var body = document.getElementsByTagName('body')[0];
-//    var script = document.createElement('script');
-//    script.setAttribute('src', 'http://wall2003.github.io/prepare.container.js');
-//    body.appendChild(script);
 })();
