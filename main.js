13334bc770ad8a6cbff3c72790d109251265179f
Pavel Fokin <2268651@gmail.com>
2014-11-04 22:24:49 +0300

    test

--- main.js
+++ main.js
@@ -1,44 +1,18 @@
 (function(){
+    var head = document.getElementsByTagName('head')[0];
     //default theme
     var theme = 'Default';
     //add css
-    var link = document.createElement('link');
-    link.setAttribute('href', 'http://wall2003.github.io/Themes/' + theme +'/css/bootstrap.min.css');
-    link.setAttribute('rel', 'stylesheet');
-    head.appendChild(link);
 
-    link = document.createElement('link');
-    link.setAttribute('href', 'http://wall2003.github.io/Themes/' + theme +'/css/webrunes.css');
-    link.setAttribute('rel', 'stylesheet');
-    head.appendChild(link);
+    document.write("<link src='http://wall2003.github.io/Themes/" + theme + "/css/bootstrap.min.css' rel='stylesheet'/>");
+    document.write("<link src='http://wall2003.github.io/Themes/" + theme + "/css/webrunes.css' rel='stylesheet'/>");
+    document.write("<link src='http://wall2003.github.io/Themes/" + theme + "/ico/favicon.ico' rel='shortcut icon'/>");
 
-    link = document.createElement('link');
-    link.setAttribute('href', 'http://wall2003.github.io/Themes/' + theme +'/ico/favicon.ico');
-    link.setAttribute('rel', 'shortcut icon');
-    head.appendChild(link);
+    document.write("<link src='http://wall2003.github.io/Themes/" + theme + "/ico/apple-touch-icon-144-precomposed.png' rel='apple-touch-icon-precomposed' sizes='144x144'/>");
+    document.write("<link src='http://wall2003.github.io/Themes/" + theme + "/ico/apple-touch-icon-114-precomposed.png' rel='apple-touch-icon-precomposed' sizes='114x114'/>");
+    document.write("<link src='http://wall2003.github.io/Themes/" + theme + "/ico/apple-touch-icon-72-precomposed.png' rel='apple-touch-icon-precomposed' sizes='72x72'/>");
+    document.write("<link src='http://wall2003.github.io/Themes/" + theme + "/ico/apple-touch-icon-57-precomposed.png' rel='apple-touch-icon-precomposed'/>");
 
-    link = document.createElement('link');
-    link.setAttribute('rel', 'apple-touch-icon-precomposed');
-    link.setAttribute('sizes', '144x144');
-    link.setAttribute('href', 'http://wall2003.github.io/Themes/' + theme +'/ico/apple-touch-icon-144-precomposed.png');
-    head.appendChild(link);
-
-    link = document.createElement('link');
-    link.setAttribute('rel', 'apple-touch-icon-precomposed');
-    link.setAttribute('sizes', '114x114');
-    link.setAttribute('href', 'http://wall2003.github.io/Themes/' + theme +'/ico/apple-touch-icon-114-precomposed.png');
-    head.appendChild(link);
-
-    link = document.createElement('link');
-    link.setAttribute('rel', 'apple-touch-icon-precomposed');
-    link.setAttribute('sizes', '72x72');
-    link.setAttribute('href', 'http://wall2003.github.io/Themes/' + theme +'/ico/apple-touch-icon-72-precomposed.png');
-    head.appendChild(link);
-
-    link = document.createElement('link');
-    link.setAttribute('rel', 'apple-touch-icon-precomposed');
-    link.setAttribute('href', 'http://wall2003.github.io/Themes/' + theme +'/ico/apple-touch-icon-57-precomposed.png');
-    head.appendChild(link);
 
     //scripts
     document.write("<script src='//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>");
