d3bf2574037b0d4e03c238192e09c2f64a6de807
Pavel Fokin <2268651@gmail.com>
2014-11-24 23:22:29 +0300

    test

--- Widgets/blogs-widget.js
+++ Widgets/blogs-widget.js
@@ -2,14 +2,14 @@
 
 angular
     .module('appBlogs', [])
-    .config(function($sceDelegateProvider){
-        $sceDelegateProvider.resourceUrlWhitelist([
-            // Allow same origin resource loads.
-            'self',
-            // Allow loading from outer templates domain.
-            'http://wall2003.github.io/Widgets/**'
-        ]);
-    })
+//    .config(function($sceDelegateProvider){
+//        $sceDelegateProvider.resourceUrlWhitelist([
+//            // Allow same origin resource loads.
+//            'self',
+//            // Allow loading from outer templates domain.
+//            'http://wall2003.github.io/Widgets/**'
+//        ]);
+//    })
     .directive('blogsWidget', function(){
         return {
             restrict: 'A',
